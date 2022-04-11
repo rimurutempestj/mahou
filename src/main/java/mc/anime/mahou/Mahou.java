3d0f970368b92080c6dccdc85bea48ef9a7b17bc
rimurutempestj <93257307+rimurutempestj@users.noreply.github.com>
2022-04-11 23:29:38 +0900

    Add files via upload

--- src/main/java/mc/anime/mahou/Mahou.java
+++ src/main/java/mc/anime/mahou/Mahou.java
@@ -14,12 +14,12 @@ public final class Mahou extends JavaPlugin implements CommandExecutor {
     @Override
     public void onEnable() {
         // Plugin startup logic
-        System.out.println("Mahou Plugin Enabled");
+        System.out.println("Mahou 0.0.2 Loaded");
     }
 
     @Override
     public boolean onCommand(CommandSender sd, Command cd, String lb, String[] ag) {
-        if (lb.equalsIgnoreCase("wingarudiumrebiousa")) {
+        if (lb.equalsIgnoreCase("wingarudiumrebiousa")) { //fly another player
             if (sd instanceof Player) {
                 Player p = (Player) sd;
 
@@ -28,6 +28,20 @@ public final class Mahou extends JavaPlugin implements CommandExecutor {
 
                     tg.addPotionEffect(new PotionEffect(PotionEffectType.LEVITATION, 10, 60,
                             false, false));
+                }
+            }
+        } else if(lb.equalsIgnoreCase("heal")) { //heal self or another player
+            if (sd instanceof Player) {
+                Player p = (Player) sd;
+
+                if (ag.length == 1) {
+                    Player tg = Bukkit.getPlayer(ag[0]);
+
+                    tg.addPotionEffect(new PotionEffect(PotionEffectType.HEAL, 1, 2, false, false));
+                } else if (ag.length == 0) {
+                    Player pl = (Player) sd;
+
+                    pl.addPotionEffect(new PotionEffect(PotionEffectType.HEAL, 1, 2, false, false));
                 }
             }
         }
